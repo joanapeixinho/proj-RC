0c994c766161e2548d35ef6ce0fbe78b3f6d2e57
Peixinho <joanapeixinho0@gmail.com>
2023-12-08 17:58:01 +0000

    Remove unnecessary comments in user_data.cpp

--- src/Server/user_data.cpp
+++ src/Server/user_data.cpp
@@ -11,10 +11,6 @@ const std::string& UserData::getPassword() const {
     return password;
 }
 
-/* Login. Each user is identified by a user ID UID, a 6-digit IST student number, and a
-password composed of 8 alphanumeric (only letters and digits) characters. When the
-AS receives a login request it will inform of a successful or incorrect login attempt
-or, in case the UID was not present at the AS, register a new user. */
 
 void UserData::login(uint32_t id, const std::string& password) {
     
