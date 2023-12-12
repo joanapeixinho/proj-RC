118bd31a92de90587d621b9419f649efb32913f2
Sepremo <miguelfernandes233@gmail.com>
2023-12-12 22:45:00 +0000

    Fix user id validation in parse_user_id function

--- src/Client/commands.cpp
+++ src/Client/commands.cpp
@@ -848,8 +848,13 @@ bool is_numeric(std::string& str) {
 
 uint32_t parse_user_id(std::string& args) {
   size_t converted = 0;
+  if(args.length() != USER_ID_STR_LEN){
+    throw std::runtime_error("invalid user id");
+  }
+
   long user_id = static_cast<uint32_t>(std::stoi(args, &converted, 10));
-  if (converted != args.length() || user_id != USER_ID_MAX) {
+  if (converted != args.length() || user_id <= 0 ||
+      user_id != USER_ID_MAX) {
     throw std::runtime_error("invalid user id");
   }
 
