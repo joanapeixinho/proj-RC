d36ebc8cc9861da0a91a13917ec4df2035216fee
Sepremo <miguelfernandes233@gmail.com>
2023-12-13 00:58:26 +0000

    TODO in asset file path constant in OpenAuctionCommand

--- src/Client/commands.cpp
+++ src/Client/commands.cpp
@@ -329,7 +329,7 @@ void OpenAuctionCommand::handle(std::string args, UserState& state) {
   packet_out.start_value = start_value;
   packet_out.time_active = timeactive;
   packet_out.file_name = asset_file_name;
-  // We assume the file is in the ASSETS directory
+  // TODO: Remove this constant and leave the asset_file_name as the path
   packet_out.file_path = std::filesystem::path(ASSETS_RELATIVE_DIRERCTORY) / asset_file_name;
 
 
