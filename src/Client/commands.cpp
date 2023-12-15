6eb31ee3a4ec18b258e54cabd2642f11f69cc167
Sepremo <miguelfernandes233@gmail.com>
2023-12-15 19:30:49 +0000

    Refactor printBidsInfo function to improve readability

--- src/Client/commands.cpp
+++ src/Client/commands.cpp
@@ -819,8 +819,8 @@ void printBidsInfo(const std::vector<Bid> bids) {
     for (const auto bid : bids) {
         std::cout << "Bidder User ID: " << bid.bidder_user_id 
                   << " Value bidded: " << bid.bid_value << std::endl;
-        std::cout << "Date Time: " << bid.date_time
-                  << " Seconds since auction start: " << bid.sec_time << std::endl;
+        std::cout << "Date Time: " << bid.date_time << std::endl;
+        std::cout << " Seconds since auction start: " << bid.sec_time << std::endl;
         std::cout << "---------------------------------------" << std::endl;
     }
 }
